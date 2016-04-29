64779d31d13b8fc369ab6695ec494a6332f702f8
Pål-Erik Martinsen <palerikm@gmail.com>
2016-04-29 11:22:19 +0200

    Iproved basj with arguments.

--- nattorture.sh
+++ nattorture.sh
@@ -1,7 +1,47 @@
 #!/bin/bash
-echo "Start, stop, transactions, failed, max rtt, min rtt, avg rtt, retries, client sent, server sent, start port, stop port" > nattorture.csv
-for i in {1..200}
+
+#usage ./nattorture.sh [interface[] [ip] [number of runs] [outfile]
+
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# Initialize our own variables:
+
+interface="eth0"
+ip=""
+runs=10
+output_file="out.csv"
+test_name="test"
+
+while getopts "h?i:f:r:o:t:" opt; do
+    case "$opt" in
+    h|\?)
+        show_help
+        exit 0
+        ;;
+    r)  runs=$OPTARG
+        ;;
+    i)  interface=$OPTARG
+        ;;
+    f)  output_file=$OPTARG
+        ;;
+    t)  test_name=$OPTARG
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "$1" = "--" ] && shift
+
+ip=$@
+echo "interface=$interface, output_file='$output_file',runs='$runs', ip: $ip"
+
+
+printf "Name, run, Start, stop, transactions, failed, max rtt, min rtt, avg rtt, retries, client sent, server sent, start port, stop port\n" > $output_file
+for (( i=1; i<=$runs; i++ ))
 do
   echo "Run $i"
- build/dist/bin/stunclient -i en0 192.168.1.117 -j 60 --csv >> nattorture.csv
+  printf "$test_name, $i, " >> $output_file
+ build/dist/bin/stunclient -i $interface $ip -j 60 --csv >> $output_file
 done
